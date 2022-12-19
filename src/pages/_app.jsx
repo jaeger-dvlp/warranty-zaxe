c63c37fc6c19da6fb44b0b60b38c807afd6c4b03
JAEGER <omer.kayalar0101@icloud.com>
2022-12-19 12:11:00 +0300

    feat: import header

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,8 +3,15 @@ import '@/src/styles/globals.css';
 import { appWithTranslation } from 'next-i18next';
 import nextI18nextConfig from '@/next-i18next.config';
 
+import Header from '@/src/components/misc/Header';
+
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default appWithTranslation(App, nextI18nextConfig);
