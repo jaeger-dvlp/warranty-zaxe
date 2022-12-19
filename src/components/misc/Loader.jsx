cf62685c232506ce83ad00cdfccfa0d458340667
JAEGER <omer.kayalar0101@icloud.com>
2022-12-19 17:14:07 +0300

    feat: create loader

---
+++ src/components/misc/Loader.jsx
@@ -0,0 +1,40 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+
+function Loader() {
+  const { locale, asPath } = useRouter();
+
+  const [loader, setLoader] = React.useState({
+    isActive: true,
+    inHTML: true,
+  });
+
+  const HideLoader = () => {
+    setTimeout(() => setLoader({ isActive: false, inHTML: true }), 1000);
+    setTimeout(() => setLoader({ isActive: false, inHTML: false }), 1500);
+  };
+
+  const StartLoader = () => {
+    setLoader({
+      isActive: true,
+      inHTML: true,
+    });
+    HideLoader();
+  };
+
+  React.useEffect(() => StartLoader(), [locale, asPath]);
+
+  return (
+    loader.inHTML && (
+      <div
+        className={`${
+          loader.isActive
+            ? 'transition-none duration-[0ms] !opacity-100 !visible'
+            : 'transition-all duration-500 !opacity-0 !invisible'
+        } zaxe-app-loader fixed !z-[99999999999999] top-0 bg-white left-0 w-screen h-screen`}
+      />
+    )
+  );
+}
+
+export default Loader;
