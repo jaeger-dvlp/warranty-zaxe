7e36404db5eea2b7b3f513072ebc680f2aaaf925
JAEGER <omer.kayalar0101@icloud.com>
2022-12-19 17:15:31 +0300

    feat: app layout

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import '@/src/styles/globals.css';
 import '@/src/styles/fonts/fonts.css';
 import { appWithTranslation } from 'next-i18next';
-import nextI18nextConfig from '@/next-i18next.config';
 
 import Header from '@/src/components/misc/Header';
+import AppWrapper from '@/src/contexts/AppWrapper';
+import AlertPopup from '../components/popups/AlertPopup';
+import Loader from '../components/misc/Loader';
 
 function App({ Component, pageProps }) {
   return (
-    <>
+    <AppWrapper>
       <Header />
+      <Loader />
+      <AlertPopup />
       <Component {...pageProps} />
-    </>
+    </AppWrapper>
   );
 }
 
-export default appWithTranslation(App, nextI18nextConfig);
+export default appWithTranslation(App);
