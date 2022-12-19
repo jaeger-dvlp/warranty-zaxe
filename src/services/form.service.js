52233226a7fe401a5b1359221966c235f0ed683d
JAEGER <omer.kayalar0101@icloud.com>
2022-12-19 17:11:41 +0300

    feat: create service

---
+++ src/services/form.service.js
@@ -0,0 +1,16 @@
+import API from '@/src/configs/api.config';
+
+class FormService {
+  constructor() {
+    this.api = API;
+    this.body = {};
+  }
+
+  async sendWarrantyForm(body) {
+    // this.api.post('/forms/warranty', body);
+    this.body = body;
+    console.log(this.body);
+  }
+}
+
+export default new FormService();
