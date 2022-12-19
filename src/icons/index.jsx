4812e6903dbf9355f886693a77afd28a4e53c970
JAEGER <omer.kayalar0101@icloud.com>
2022-12-19 17:11:50 +0300

    feat: Create Icon component

---
+++ src/icons/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { AiOutlineLoading } from 'react-icons/ai';
+import { BsCheckCircleFill, BsExclamationCircleFill } from 'react-icons/bs';
+
+const Icons = {
+  error: BsExclamationCircleFill,
+  loading: AiOutlineLoading,
+  check: BsCheckCircleFill,
+};
+
+function Icon({ name, className }) {
+  const Component = Icons[name];
+  return <Component className={className} />;
+}
+
+export default Icon;
