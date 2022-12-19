a15c224676b5a3b3bb9c9f656a691bae90d03e10
JAEGER <omer.kayalar0101@icloud.com>
2022-12-19 12:11:14 +0300

    style: theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        app: '1000px',
+      },
+      colors: {
+        zaxe: '#009ade',
+        'zaxe-black': '#060608',
+        'zaxe-white': '#f6f6f6',
+      },
+      fontFamily: {
+        zaxe: 'Mark',
+        mark: 'Mark',
+      },
+      maxHeight: {
+        max3xl: '500px',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 };
