548a23150ff7e3d6780cc4bf9d617e0ae1cc52cb
JAEGER <omer.kayalar0101@icloud.com>
2022-12-19 13:03:29 +0300

    feat: create welcome side

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -46,10 +46,16 @@ function Home() {
           href="https://warranty.zaxe.com"
         />
       </Head>
-
-      <section className="flex items-center justify-center w-full p-0 m-0 bg-zinc-100">
-        <section className="grid w-full grid-cols-1 p-5 place-content-start place-items-start max-w-app">
-          {t('hey')}
+      <section className="flex items-center justify-center w-full p-5 m-0 mt-10 font-zaxe">
+        <section className="flex items-start justify-start w-full max-w-app">
+          <section className="grid w-full max-w-md grid-cols-1 p-5 xl:max-w-2xl lg:max-w-2xl place-content-start place-items-start ">
+            <h1 className="text-xl font-semibold xl:text-3xl lg:text-3xl text-zinc-500">
+              {t('pages.home.welcome.heading')}
+            </h1>
+            <p className="text-xs xl:text-sm lg:text-sm text-zinc-500">
+              {t('pages.home.welcome.description')}
+            </p>
+          </section>
         </section>
       </section>
     </>
