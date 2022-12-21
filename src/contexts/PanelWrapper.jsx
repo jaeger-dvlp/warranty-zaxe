2244e2665222dcb8d9de5e04d86b271b478a9fda
JAEGER <omer.kayalar0101@icloud.com>
2022-12-21 17:10:08 +0300

    rev: handle name in navbar

--- src/contexts/PanelWrapper.jsx
+++ src/contexts/PanelWrapper.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useTranslation } from 'next-i18next';
 import { BiAddToQueue, BiEdit, BiListUl } from 'react-icons/bi';
 
 import Edit from '@/src/components/panels/Edit';
