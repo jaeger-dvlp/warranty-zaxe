8d2abd3bad292bc45029ac2118b25dbf4ea2f830
JAEGER <omer.kayalar0101@icloud.com>
2022-12-22 13:42:49 +0300

    feat: handle auth on panel change

--- src/components/misc/NavBar.jsx
+++ src/components/misc/NavBar.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BsKeyFill } from 'react-icons/bs';
 import { useTranslation } from 'next-i18next';
+import { useAppContext } from '@/src/contexts/AppWrapper';
 import { usePanelContext } from '@/src/contexts/PanelWrapper';
+import { useUser } from '@supabase/auth-helpers-react';
 
 function NavBar() {
   const { activePanel, setActivePanel, panels } = usePanelContext();
@@ -24,12 +26,22 @@ function NavBar() {
 
 function PageButtons({ pageContext: { panels, setActivePanel, activePanel } }) {
   const { t } = useTranslation();
+  const user = useUser();
+  const { activateAuthPopup } = useAppContext();
+  const changePanel = (index) => {
+    const requestedPanel = panels[index];
+
+    if (requestedPanel.requiresAuth && !user) return activateAuthPopup();
+
+    return setActivePanel(index);
+  };
+
   return (
     <div className="flex flex-wrap items-start justify-start w-full gap-5 max-w-fit">
       {panels.map(({ name, icon: Icon }, index) => (
         <button
           key={`button-${name.toLowerCase()}`}
-          onClick={() => setActivePanel(index)}
+          onClick={() => changePanel(index)}
           className={`${
             index === activePanel
               ? 'bg-zaxe/20 text-zaxe border border-sky-300'
@@ -46,9 +58,10 @@ function PageButtons({ pageContext: { panels, setActivePanel, activePanel } }) {
 }
 
 function AuthButton() {
+  const { activateAuthPopup } = useAppContext();
   return (
     <button
-      onClick={() => null}
+      onClick={() => activateAuthPopup()}
       className="bg-zinc-100 border-zinc-200 hover:border-zinc-300 hover:bg-zinc-200 border text-zinc-600 flex items-center justify-center gap-1 p-1 px-2 font-semibold text-center transition-all duration-100  rounded-md ring-0 !ring-sky-400 active:ring"
       type="button"
     >
