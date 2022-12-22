5049f301d4926dd9f713728c3fbfa67e7859263f
JAEGER <omer.kayalar0101@icloud.com>
2022-12-22 17:20:27 +0300

    feat: complete all funcs

--- src/components/popups/AuthPopup.jsx
+++ src/components/popups/AuthPopup.jsx
@@ -1,30 +1,60 @@
 import React from 'react';
 import { useTranslation } from 'next-i18next';
-import firstUpperCase from '@/src/utils/FirstUpperCase';
 import { useAppContext } from '@/src/contexts/AppWrapper';
 import { BsLockFill } from 'react-icons/bs';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
 
 const Classes = {
   input: `w-full p-2 py-3 h-[3.5rem] peer transition-all duration-150 font-semibold bg-white border-2 rounded-md !outline-none !ring-2 ring-transparent focus:valid:ring-zaxe focus:invalid:ring-pink-400 text-zinc-600 !border-slate-200`,
   label: `absolute pointer-events-none top-0 px-2 py-0.5 font-semibold duration-150 scale-75 -translate-y-1/2 border rounded-md tranisition-all peer-placeholder-shown:border-transparent border-slate-200 peer-focus:scale-75 peer-placeholder-shown:scale-100 peer-focus:border-slate-200 text-slate-500 peer-focus:peer-valid:text-zaxe peer-focus:peer-invalid:text-pink-400 peer-focus:top-0 peer-placeholder-shown:top-7 peer-focus:-translate-x-[10%] max-w-[95%] -translate-x-[10%] peer-placeholder-shown:-translate-x-0 !left-2 placeholder-shown:bg-transparent peer-focus:bg-white bg-white`,
 };
 
-function LoginForm({ formPrefix, setActiveTab }) {
+function LoginForm({ formPrefix = 'auth-login-form', setActiveTab }) {
   const { t } = useTranslation();
-  const { activateAlertPopup } = useAppContext();
+  const supabase = useSupabaseClient();
+  const { activateAlertPopup, updateAlertPopup, deactivateAuthPopup } =
+    useAppContext();
 
   const [requestBody, setRequestBody] = React.useState({
     emailAddress: '',
     password: '',
   });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const body = {
-      email: requestBody.emailAddress,
-      password: requestBody.password,
-    };
-    console.log(body);
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      activateAlertPopup({
+        status: 'loading',
+        message: t('popups.auth.loading.logging-in'),
+      });
+
+      const body = {
+        email: requestBody.emailAddress,
+        password: requestBody.password,
+      };
+
+      return await supabase.auth.signInWithPassword(body).then(({ error }) => {
+        if (error) throw error;
+
+        deactivateAuthPopup();
+        return updateAlertPopup({
+          status: 'success',
+          message: t('popups.auth.success.logged-in'),
+        });
+      });
+    } catch (error) {
+      if (error.message === 'Invalid login credentials') {
+        return updateAlertPopup({
+          status: 'error',
+          message: t('popups.auth.errors.invalid-credentials'),
+        });
+      }
+
+      return updateAlertPopup({
+        status: 'error',
+        message: error.message || t('popups.global.errors.error-occurred'),
+      });
+    }
   };
 
   const HandleChange = (e) => {
@@ -34,7 +64,7 @@ function LoginForm({ formPrefix, setActiveTab }) {
       if (requestBody[field] !== undefined) {
         return setRequestBody({
           ...requestBody,
-          [field]: firstUpperCase(value),
+          [field]: value,
         });
       }
       return activateAlertPopup({
@@ -76,7 +106,6 @@ function LoginForm({ formPrefix, setActiveTab }) {
           className={Classes.input}
           id={`${formPrefix}-password`}
           placeholder=" "
-          pattern="\D{3,}"
           required
           onChange={HandleChange}
         />
@@ -110,18 +139,42 @@ function ForgotPasswordForm({
   setActiveTab,
 }) {
   const { t } = useTranslation();
-  const { activateAlertPopup } = useAppContext();
+  const supabase = useSupabaseClient();
+  const { activateAlertPopup, updateAlertPopup } = useAppContext();
 
   const [requestBody, setRequestBody] = React.useState({
     emailAddress: '',
   });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const body = {
-      email: requestBody.emailAddress,
-    };
-    console.log(body);
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const body = {
+        email: requestBody.emailAddress,
+      };
+
+      activateAlertPopup({
+        status: 'loading',
+        message: t('popups.auth.loading.resetting-password'),
+      });
+      await supabase.auth
+        .resetPasswordForEmail(body.email, {
+          redirectTo: `/reset`,
+        })
+        .then(({ error }) => {
+          if (error) throw error;
+
+          return updateAlertPopup({
+            status: 'success',
+            message: t('popups.auth.success.reset-password'),
+          });
+        });
+    } catch (error) {
+      updateAlertPopup({
+        status: 'error',
+        message: error.message || t('popups.global.errors.error-occurred'),
+      });
+    }
   };
 
   const HandleChange = (e) => {
@@ -131,7 +184,7 @@ function ForgotPasswordForm({
       if (requestBody[field] !== undefined) {
         return setRequestBody({
           ...requestBody,
-          [field]: firstUpperCase(value),
+          [field]: value,
         });
       }
       return activateAlertPopup({
@@ -211,12 +264,12 @@ function AuthPopup() {
       >
         <div
           onClick={(e) => e.stopPropagation()}
-          className={`grid relative w-full auth-popup max-w-sm grid-cols-1 gap-10 p-5 transition-all duration-[0.3s] bg-white rounded-xl shadow-2xl shadow-black/40 place-content-between place-items-center ${
+          className={`grid relative w-full auth-popup max-w-md grid-cols-1 gap-10 p-5 transition-all duration-[0.3s] bg-white rounded-xl shadow-2xl shadow-black/40 place-content-between place-items-center ${
             isActive ? 'translate-y-0' : 'translate-y-10'
           }`}
         >
           <div className="flex items-center justify-center w-full">
-            <span className="relative flex items-center justify-center w-12 h-12 p-2 rounded-full bg-zinc-200">
+            <span className="relative flex items-center justify-center w-12 h-12 p-2 rounded-lg bg-zinc-200">
               <BsLockFill className="w-full h-full text-zinc-700" />
             </span>
           </div>
