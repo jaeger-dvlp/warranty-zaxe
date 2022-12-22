4c5e2468a7c8b7c9f66090ed1a2827fd8af2ff4d
JAEGER <omer.kayalar0101@icloud.com>
2022-12-22 13:41:44 +0300

    import components

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,21 +4,26 @@ import '@/src/styles/fonts/fonts.css';
 import { appWithTranslation } from 'next-i18next';
 
 import AppWrapper from '@/src/contexts/AppWrapper';
+import SupaWrapper from '@/src/contexts/SupaWrapper';
 import PanelWrapper from '@/src/contexts/PanelWrapper';
 
 import Header from '@/src/components/misc/Header';
 import Loader from '@/src/components/misc/Loader';
+import AuthPopup from '@/src/components/popups/AuthPopup';
 import AlertPopup from '@/src/components/popups/AlertPopup';
 
 function App({ Component, pageProps }) {
   return (
     <AppWrapper>
-      <PanelWrapper>
-        <Header />
-        <Loader />
-        <AlertPopup />
-        <Component {...pageProps} />
-      </PanelWrapper>
+      <SupaWrapper pageProps={pageProps}>
+        <PanelWrapper>
+          <Header />
+          <Loader />
+          <AlertPopup />
+          <AuthPopup />
+          <Component {...pageProps} />
+        </PanelWrapper>
+      </SupaWrapper>
     </AppWrapper>
   );
 }
