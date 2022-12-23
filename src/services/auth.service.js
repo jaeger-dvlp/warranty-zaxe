9cb4c2101acf78f4d0187de373a6fd74a4306420
JAEGER <omer.kayalar0101@icloud.com>
2022-12-23 10:41:10 +0300

    feat: Create auth service

---
+++ src/services/auth.service.js
@@ -0,0 +1,75 @@
+class AuthService {
+  constructor() {
+    this.supabase = null;
+  }
+
+  async Login({ supabase, email, password }) {
+    try {
+      return new Promise((resolve, reject) => {
+        setTimeout(async () => {
+          this.supabase = supabase;
+          const data = await this.supabase.auth.signInWithPassword({
+            email,
+            password,
+          });
+
+          if (data.error) {
+            reject(data.error);
+          }
+
+          resolve(data);
+        }, 1000);
+      });
+    } catch (error) {
+      return setTimeout(() => {
+        throw new Error(error?.message || error);
+      }, 1000);
+    }
+  }
+
+  async Logout(supabase) {
+    try {
+      return new Promise((resolve, reject) => {
+        setTimeout(async () => {
+          this.supabase = supabase;
+          const data = await this.supabase.auth.signOut();
+
+          if (data.error) {
+            reject(data.error);
+          }
+
+          resolve(data);
+        }, 1000);
+      });
+    } catch (error) {
+      throw new Error(error?.message || error);
+    }
+  }
+
+  async AddUser({ supabase, email, password }) {
+    try {
+      return new Promise((resolve, reject) => {
+        setTimeout(async () => {
+          this.supabase = supabase;
+          const data = await this.supabase.auth.signUp({
+            email,
+            password,
+            redirectTo: `${
+              window?.location?.origin || 'https://warranty-zaxe.vercel.app'
+            }/resetpassword`,
+          });
+
+          if (data.error) {
+            reject(data.error);
+          }
+
+          resolve(data);
+        }, 1000);
+      });
+    } catch (error) {
+      throw new Error(error?.message || error);
+    }
+  }
+}
+
+export default new AuthService();
