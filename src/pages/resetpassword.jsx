2f51f75afec43ed57edb6190e0af2da32094ee01
JAEGER <omer.kayalar0101@icloud.com>
2022-12-23 14:33:30 +0300

    feat: Creat Reset Password page

--- src/pages/resetpassword.jsx
+++ src/pages/resetpassword.jsx
@@ -1,58 +1,176 @@
 import React from 'react';
-import { useRouter } from 'next/router';
-
+import Link from 'next/link';
+import Classes from '@/src/utils/Classes';
 import { useTranslation } from 'next-i18next';
-import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import Label from '@/src/components/misc/Label';
+import AuthService from '@/src/services/auth.service';
+import { useAppContext } from '@/src/contexts/AppWrapper';
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-function ResetPassword() {
+function ResetPassword({
+  formName = 'reset-password-form',
+  formPrefix = 'reset-password',
+}) {
+  const session = useSession();
   const { t } = useTranslation();
-
-  const Router = useRouter();
   const supabase = useSupabaseClient();
+  const { activateAlertPopup, updateAlertPopup } = useAppContext();
 
-  const [session, setSession] = React.useState(null);
+  const [requestBody, setRequestBody] = React.useState({
+    password: '',
+    passwordConfirmation: '',
+  });
 
-  React.useEffect(() => {
-    const init = async () => {
-      const { data } = await supabase.auth.getSession();
+  const resetPassword = async (e) => {
+    try {
+      e.preventDefault();
+      activateAlertPopup({
+        status: 'loading',
+        message: t('popups.reset-pwd.loading.resetting-password'),
+      });
 
-      if (data) {
-        setSession(data);
+      const {
+        user: { email },
+      } = session;
+
+      const { password, passwordConfirmation } = requestBody;
+      if (password !== passwordConfirmation) {
+        return updateAlertPopup({
+          status: 'error',
+          message: t('popups.reset-pwd.errors.passwords-not-match'),
+        });
       }
-    };
 
-    supabase.auth.onAuthStateChange((_event, theSession) => {
-      setSession(theSession);
-    });
-
-    init();
-  }, []);
-
-  React.useEffect(() => {
-    if (session !== null && !session.session) {
-      Router.push('/');
+      return await AuthService.ResetPWD({
+        email,
+        password,
+        supabase,
+      })
+        .then(() => {
+          updateAlertPopup({
+            status: 'success',
+            message: t('popups.reset-pwd.success.password-reset'),
+          });
+        })
+        .catch((err) => {
+          throw err;
+        });
+    } catch (error) {
+      return updateAlertPopup({
+        status: 'error',
+        message: error?.message || t('popups.global.errors.error-occurred'),
+      });
     }
-  }, [session]);
+  };
 
-  if (!session?.session) return null;
+  const HandleChange = (e) => {
+    try {
+      const { value } = e.target;
+      const field = e.target.id.replace(`${formPrefix}-`, '');
+      if (requestBody[field] !== undefined) {
+        return setRequestBody({
+          ...requestBody,
+          [field]: value,
+        });
+      }
+      return activateAlertPopup({
+        message: t('popups.global.errors.invalid-field'),
+        status: 'error',
+      });
+    } catch (error) {
+      return activateAlertPopup({
+        message: error.message,
+        status: 'error',
+      });
+    }
+  };
 
-  return (
-    <section className="grid w-full grid-cols-1 font-zaxe place-content-start place-items-center">
-      <section className="flex items-start justify-start w-full py-5 my-5 max-w-app">
-        <section className="grid w-full max-w-md grid-cols-1 gap-2 p-5 xl:max-w-2xl lg:max-w-2xl place-content-start place-items-start ">
-          <h1 className="text-xl font-semibold xl:text-3xl lg:text-3xl text-zinc-500">
-            {t('pages.resetpwd.welcome.heading')}
-          </h1>
-          <p className="text-xs xl:text-sm lg:text-sm text-zinc-500">
-            {t('pages.resetpwd.welcome.description')}
+  if (!session) {
+    return (
+      <section className="grid w-full grid-cols-1 font-zaxe place-content-start place-items-center">
+        <Welcome />
+        <section className="grid items-start justify-start w-full grid-cols-1 gap-2 px-5 place-content-start place-items-start max-w-app">
+          <p className="text-sm text-left text-zinc-500">
+            {t('forms.reset-pwd.buttons.not-logged-in')}
           </p>
+          <Link
+            className="p-2 px-5 text-sm text-white transition-all duration-200 border-b-4 active:mb-[4px] mb-0 rounded-md active:border-b-0 active:translate-y-[4px] bg-zaxe hover:bg-sky-600 border-b-sky-700"
+            href="/"
+          >
+            {t('forms.reset-pwd.buttons.go-back')}
+          </Link>
         </section>
       </section>
-      <section className="flex items-start justify-start w-full max-w-app">
-        <div className="w-full p-5 bg-white border shadow-xl rounded-xl border-zinc-100">
-          Yo yo yo, mr white.
-        </div>
+    );
+  }
+
+  return (
+    session && (
+      <section className="grid w-full grid-cols-1 font-zaxe place-content-start place-items-center">
+        <Welcome />
+        <section className="flex items-start justify-start w-full max-w-app">
+          <div className="flex items-center justify-center w-full p-5 bg-white border shadow-xl rounded-xl border-zinc-100">
+            <form
+              autoComplete="on"
+              onSubmit={(e) => resetPassword(e)}
+              className={`grid ${formName} w-full grid-cols-2 gap-5`}
+            >
+              <section className="relative w-full xl:col-span-1 lg:col-span-1 col-span-full">
+                <input
+                  type="password"
+                  autoComplete="on"
+                  className={Classes.input}
+                  id={`${formPrefix}-password`}
+                  placeholder=" "
+                  required
+                  onChange={(e) => HandleChange(e)}
+                />
+                <Label required htmlFor={`${formPrefix}-password`}>
+                  {t('forms.reset-pwd.inputs.pwd')}
+                </Label>
+              </section>
+              <section className="relative w-full xl:col-span-1 lg:col-span-1 col-span-full">
+                <input
+                  type="password"
+                  autoComplete="on"
+                  className={Classes.input}
+                  id={`${formPrefix}-passwordConfirmation`}
+                  placeholder=" "
+                  required
+                  onChange={(e) => HandleChange(e)}
+                />
+                <Label required htmlFor={`${formPrefix}-passwordConfirmation`}>
+                  {t('forms.reset-pwd.inputs.pwd-confirm')}
+                </Label>
+              </section>
+              <section className="relative flex items-center justify-end w-full col-span-full">
+                <button
+                  type="submit"
+                  className="p-2 px-5 text-white transition-all duration-200 border-b-4 active:mb-[4px] mb-0 rounded-md active:border-b-0 active:translate-y-[4px] bg-zaxe hover:bg-sky-600 border-b-sky-700"
+                >
+                  {t('forms.reset-pwd.buttons.update')}
+                </button>
+              </section>
+            </form>
+          </div>
+        </section>
+      </section>
+    )
+  );
+}
+
+function Welcome() {
+  const { t } = useTranslation();
+  return (
+    <section className="flex items-start justify-start w-full py-5 my-5 max-w-app">
+      <section className="grid w-full max-w-md grid-cols-1 gap-2 p-5 xl:max-w-2xl lg:max-w-2xl place-content-start place-items-start ">
+        <h1 className="text-xl font-semibold xl:text-3xl lg:text-3xl text-zinc-500">
+          {t('pages.resetpwd.welcome.heading')}
+        </h1>
+        <p className="text-xs xl:text-sm lg:text-sm text-zinc-500">
+          {t('pages.resetpwd.welcome.description')}
+        </p>
       </section>
     </section>
   );
