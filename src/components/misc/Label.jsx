05f8f20364989cf906aeddf4f108e8bedf04356c
JAEGER <omer.kayalar0101@icloud.com>
2022-12-23 14:36:01 +0300

    beautify 🤯

--- src/components/misc/Label.jsx
+++ src/components/misc/Label.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-
-const Classes = {
-  input: `w-full p-2 py-3 h-[3.5rem] peer transition-all duration-150 font-semibold bg-white border-2 rounded-md !outline-none !ring-2 ring-transparent focus:valid:ring-zaxe focus:invalid:ring-pink-400 text-zinc-600 !border-slate-200`,
-  label: `absolute pointer-events-none top-0 px-2 py-0.5 font-semibold duration-150 scale-75 -translate-y-1/2 border rounded-md tranisition-all peer-placeholder-shown:border-transparent border-slate-200 peer-focus:scale-75 peer-placeholder-shown:scale-100 peer-focus:border-slate-200 text-slate-500 peer-focus:peer-valid:text-zaxe peer-focus:peer-invalid:text-pink-400 peer-focus:top-0 peer-placeholder-shown:top-7 peer-focus:-translate-x-[10%] max-w-[95%] -translate-x-[10%] peer-placeholder-shown:-translate-x-0 !left-2 placeholder-shown:bg-transparent peer-focus:bg-white bg-white`,
-};
+import Classes from '@/src/utils/Classes';
 
 function Label({ htmlFor, required, children }) {
   return (
