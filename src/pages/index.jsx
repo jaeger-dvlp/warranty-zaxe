8defc7b4b94c9f0b8b18a51da707dce0400a2815
JAEGER <omer.kayalar0101@icloud.com>
2022-12-26 16:12:14 +0300

    feat: remove starter screen

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -76,18 +76,6 @@ function Home() {
           href="https://warranty.zaxe.com"
         />
       </Head>
-      <section className="flex items-center justify-center w-full p-5 m-0 my-5 font-zaxe">
-        <section className="flex items-start justify-start w-full max-w-app">
-          <section className="grid w-full max-w-md grid-cols-1 gap-2 p-5 xl:max-w-2xl lg:max-w-2xl place-content-start place-items-start ">
-            <h1 className="text-xl font-semibold xl:text-3xl lg:text-3xl text-zinc-500">
-              {t('pages.home.welcome.heading')}
-            </h1>
-            <p className="text-xs xl:text-sm lg:text-sm text-zinc-500">
-              {t('pages.home.welcome.description')}
-            </p>
-          </section>
-        </section>
-      </section>
       <section className="flex items-center justify-center w-full p-5 m-0">
         <section className="flex items-center justify-center w-full p-0 max-w-app">
           <NavBar />
