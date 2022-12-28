194aeeb7f8d89316d1448de2561c5b0a993f9414
JAEGER <omer.kayalar0101@icloud.com>
2022-12-28 11:03:54 +0300

    feat: seperate & create

---
+++ src/components/misc/TableEmpty.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function TableEmpty() {
+  return (
+    <tr className="w-full fade-in">
+      <td colSpan="11" className="">
+        <div className="relative min-h-[275px] flex items-center w-full h-full p-5">
+          <p className="sticky -translate-x-1/2 left-1/2 top-1/2">Empty.</p>
+        </div>
+      </td>
+    </tr>
+  );
+}
+
+export default TableEmpty;
