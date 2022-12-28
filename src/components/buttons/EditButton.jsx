289bb4883db12da22199aa6ac91b458df29d44b2
JAEGER <omer.kayalar0101@icloud.com>
2022-12-29 00:01:18 +0300

    feat: call editPopup

--- src/components/buttons/EditButton.jsx
+++ src/components/buttons/EditButton.jsx
@@ -1,12 +1,15 @@
+import { useAppContext } from '@/src/contexts/AppWrapper';
 import React from 'react';
 import { BiEdit } from 'react-icons/bi';
 
-function EditButton({ id }) {
+function EditButton({ item }) {
+  const { activateEditPopup } = useAppContext();
+
   return (
     <button
       type="button"
       className="flex items-center justify-center p-1 bg-white border-2 rounded-md border-zinc-300 text-zinc-600 hover:bg-zinc-100"
-      onClick={() => id}
+      onClick={() => activateEditPopup({ item })}
     >
       <BiEdit className="w-4 h-4" />
     </button>
