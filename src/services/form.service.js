d29552b8da5c2ed8fe380f3516ef33cf7a4d10d3
JAEGER <omer.kayalar0101@icloud.com>
2022-12-28 16:44:00 +0300

    feat: create form upload service

--- src/services/form.service.js
+++ src/services/form.service.js
@@ -6,10 +6,42 @@ class FormService {
     this.body = {};
   }
 
-  async sendWarrantyForm(body) {
-    // this.api.post('/forms/warranty', body); etc.
-    this.body = body;
-    console.log(this.body);
+  async sendWarrantyForm({ body, supaBase, t }) {
+    try {
+      this.body = body;
+      const { data: isExist, error } = await supaBase
+        .from('warrantyList')
+        .select('*')
+        .eq('deviceSerialNumber', this.body.deviceSerialNumber);
+
+      if (error)
+        throw Error(error?.message || 'Error checking device serial number.');
+
+      if (isExist?.length > 0)
+        throw Error(t('popups.upload.errors.serial-number-exists'));
+
+      const {
+        data: {
+          data: { fileURL },
+        },
+        error: fileErr,
+      } = await this.api.post('/warranty/upload', this.body);
+
+      if (fileErr) throw Error(fileErr?.message || 'Error uploading file.');
+
+      const { data: supaData, error: supaErr } = await supaBase
+        .from('warrantyList')
+        .insert({
+          ...this.body,
+          invoiceImage: fileURL,
+        });
+
+      if (supaErr) throw Error(supaErr?.message || 'Error inserting data.');
+
+      return supaData;
+    } catch (error) {
+      throw Error(error?.message || 'Error sending warranty form.');
+    }
   }
 }
 
