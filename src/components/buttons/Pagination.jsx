962bb4002bbecdf85b84393545c1f4da313b9684
JAEGER <omer.kayalar0101@icloud.com>
2022-12-29 11:03:38 +0300

    style: pagination buttons

--- src/components/buttons/Pagination.jsx
+++ src/components/buttons/Pagination.jsx
@@ -16,32 +16,37 @@ function Pagination({
       className="flex items-center justify-center p-0 m-5 mb-0 rounded-md shadow-lg "
       aria-label="Table navigation"
     >
-      <ul className="inline-flex items-center">
-        <li>
+      <ul className="flex items-center justify-center text-sm">
+        <li className="h-full">
           <button
             type="button"
             disabled={tableRows.name === 'TableLoading'}
             onClick={() =>
               upperRange({ supaBase, currentPage, setCurrentPage })
             }
-            className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg disabled:pointer-events-none hover:bg-gray-100 hover:text-gray-700 "
+            className="flex items-center justify-center p-2 min-h-[38px] min-w-[38px] text-gray-500 bg-white border border-gray-300 rounded-l-lg disabled:pointer-events-none hover:bg-gray-100 hover:text-gray-700 "
           >
             <BiChevronLeft className="w-5 h-5" />
           </button>
         </li>
-        <li>
-          <span className="h-full px-4 py-2 text-gray-500 bg-white border-gray-300 border-y">
+        <li className="h-full">
+          <button
+            disabled
+            type="button"
+            onClick={() => null}
+            className="flex items-center justify-center h-full p-2 min-h-[38px] min-w-[38px] text-gray-500 bg-white border-y border-gray-300 cursor-default disabled:pointer-events-none"
+          >
             {currentPage}
-          </span>
+          </button>
         </li>
-        <li>
+        <li className="h-full">
           <button
             type="button"
             disabled={tableRows.name === 'TableLoading'}
             onClick={() =>
               lowerRange({ supaBase, currentPage, setCurrentPage })
             }
-            className="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg disabled:pointer-events-none hover:bg-gray-100 hover:text-gray-700 "
+            className="flex items-center justify-center p-2 min-h-[38px] min-w-[38px] text-gray-500 bg-white border border-gray-300 rounded-r-lg disabled:pointer-events-none hover:bg-gray-100 hover:text-gray-700 "
           >
             <BiChevronRight className="w-5 h-5" />
           </button>
