47e5e750b7d32e1034a27b78140e8adede14ced3
JAEGER <omer.kayalar0101@icloud.com>
2022-12-29 10:41:14 +0300

    feat: pass onEditComplete

--- src/components/buttons/EditButton.jsx
+++ src/components/buttons/EditButton.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { BiEdit } from 'react-icons/bi';
 import { useAppContext } from '@/src/contexts/AppWrapper';
 
-function EditButton({ item }) {
+function EditButton({ item, onEditComplete }) {
   const { activateEditPopup } = useAppContext();
 
   return (
     <button
       type="button"
       className="flex items-center justify-center p-1 bg-white border-2 rounded-md border-zinc-300 text-zinc-600 hover:bg-zinc-100"
-      onClick={() => activateEditPopup({ item })}
+      onClick={() => activateEditPopup({ item, onEditComplete })}
     >
       <BiEdit className="w-4 h-4" />
     </button>
